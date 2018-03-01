61b18818284bbfc395b4ff983368eaf0c2948585
yinruiqing <yinruiqing110@gmail.com>
2018-03-01 10:45:14 +0100

    add: add user defined interface to word pair generator

--- corpus2graph/applications/wordpair_generator.py
+++ corpus2graph/applications/wordpair_generator.py
@@ -7,18 +7,19 @@ from ..word_processor import FileParser, WordPreprocessor, Tokenizer
 
 
 class WordsGenerator(object):
-    def __init__(self, window_size, file_parser='txt',
-                 xml_node_path=None, word_tokenizer='WordPunct',
+    def __init__(self, window_size, file_parser='txt', fparser=None,
+                 xml_node_path=None, word_tokenizer='WordPunct',  wtokenizer=None,
                  remove_numbers=True, remove_punctuations=True,
-                 stem_word=True, lowercase=True):
+                 stem_word=True, lowercase=True, wpreprocessor=None):
         self.window_size = window_size
-        self.file_parser = FileParser(file_parser=file_parser, xml_node_path=xml_node_path)
+        self.file_parser = FileParser(file_parser=file_parser, xml_node_path=xml_node_path, fparser=fparser)
         # TODO NOW Ruiqing user defined file_parser yield type check
         self.file_extension = file_parser
         self.word_preprocessor = WordPreprocessor(remove_numbers=remove_numbers,
                                                   remove_punctuations=remove_punctuations,
-                                                  stem_word=stem_word, lowercase=lowercase)
-        self.tokenizer = Tokenizer(word_tokenizer=word_tokenizer)
+                                                  stem_word=stem_word, lowercase=lowercase,
+                                                  wpreprocessor=wpreprocessor)
+        self.tokenizer = Tokenizer(word_tokenizer=word_tokenizer, wtokenizer=wtokenizer)
 
     def fromsent(self, sent):
         sentence_len = len(sent)
