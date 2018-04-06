26fffb6969426d3490b5321296a322f11aded959
yinruiqing <yinruiqing110@gmail.com>
2018-04-06 18:20:38 +0200

    add: graph tools test

--- corpus2graph/applications/graph_tool_test.py
+++ corpus2graph/applications/graph_tool_test.py
@@ -1,9 +1,10 @@
 __author__ = 'Ruiqing YIN'
 
 import unittest
+import time
 from corpus2graph.applications import wordpair_generator, graph_tool_wrapper, graph_generator
 from corpus2graph import FileParser, WordPreprocessor, Tokenizer, WordProcessing, \
-    SentenceProcessing, WordPairsProcessing
+    SentenceProcessing, WordPairsProcessing, util
 
 import configparser
 config = configparser.ConfigParser()
@@ -18,50 +19,51 @@ class TestGraphTool(unittest.TestCase):
     data_folder = '../test/unittest_data/'
 
     file_extension = '.txt'
-    max_window_size = 6
-    process_num = 3
+    max_window_size = 5
+    process_num = 1
     data_type = 'txt'
-    min_count = 5
-    max_vocab_size = 3
+    min_count = 0
+    max_vocab_size = 'None'
 
     def test_graph_tool_wrapper(self):
-        wg = wordpair_generator.WordsGenerator(window_size=3, file_parser='txt',
-                     xml_node_path=None, word_tokenizer='WordPunct',
-                     remove_numbers=True, remove_punctuations=True,
-                     stem_word=True, lowercase=True)
+        start_time = time.time()
+        wg = wordpair_generator.WordsGenerator(window_size=self.max_window_size, file_parser='txt',
+                     xml_node_path=None, word_tokenizer='', wtokenizer=Tokenizer.mytok,
+                     remove_numbers=False, remove_punctuations=False,
+                     stem_word=False, lowercase=False)
 
         gtw = graph_tool_wrapper.GraphToolWrapper('Test')
-        for w1,w2 in wg(self.data_folder):
+        for w1,w2 in wg(self.data_folder+'/tmp_dir/'):
             gtw.addPairs(w1, w2)
+        print(util.count_time(start_time))
+        # graph = gtw.getGraph()
+        # print('---- vertice ----')
+        # print([gtw.vname[v] for v in graph.vertices()])
+        # print('---- edge ----')
+        # print([gtw.eweight[e] for e in graph.edges()])
 
-        graph = gtw.getGraph()
-        print('---- vertice ----')
-        print([gtw.vname[v] for v in graph.vertices()])
-        print('---- edge ----')
-        print([gtw.eweight[e] for e in graph.edges()])
-
-    def test_add_edges(self):
-        gg = graph_generator.GraphGenerator(window_size=3, file_parser='txt',
-                                            xml_node_path=None, word_tokenizer='WordPunct',
-                                            remove_numbers=True, remove_punctuations=True,
-                                            stem_word=True, lowercase=True)
-        pl, wl = gg.fromfile(self.data_folder + 'AA/wiki_03.txt')
-        gtw = graph_tool_wrapper.GraphToolWrapper('Test')
-        gtw.addEdgesFromList(pl,wl)
-
-        graph = gtw.getGraph()
-        print('---- vertice ----')
-        print([gtw.vname[v] for v in graph.vertices()])
-        print('---- edge ----')
-        print([gtw.eweight[e] for e in graph.edges()])
-
-    def test_our_method(self):
-        gtw = graph_tool_wrapper.GraphToolWrapper('Test')
-        gtw.addEdgesFromFile(
-            path='../test/output/keep/encoded_edges_count_window_size_6_vocab_size_none_undirected_for_unittest.txt')
-
-        graph = gtw.getGraph()
-        print('---- vertice ----')
-        print([gtw.vname[v] for v in graph.vertices()])
-        print('---- edge ----')
-        print([gtw.eweight[e] for e in graph.edges()])
+    # def test_add_edges(self):
+    #     gg = graph_generator.GraphGenerator(window_size=3, file_parser='txt',
+    #                                         xml_node_path=None, word_tokenizer='WordPunct',
+    #                                         remove_numbers=True, remove_punctuations=True,
+    #                                         stem_word=True, lowercase=True)
+    #     pl, wl = gg.fromfile(self.data_folder + 'AA/wiki_03.txt')
+    #     gtw = graph_tool_wrapper.GraphToolWrapper('Test')
+    #     gtw.addEdgesFromList(pl,wl)
+    #
+    #     graph = gtw.getGraph()
+    #     print('---- vertice ----')
+    #     print([gtw.vname[v] for v in graph.vertices()])
+    #     print('---- edge ----')
+    #     print([gtw.eweight[e] for e in graph.edges()])
+    #
+    # def test_our_method(self):
+    #     gtw = graph_tool_wrapper.GraphToolWrapper('Test')
+    #     gtw.addEdgesFromFile(
+    #         path='../test/output/keep/encoded_edges_count_window_size_6_vocab_size_none_undirected_for_unittest.txt')
+    #
+    #     graph = gtw.getGraph()
+    #     print('---- vertice ----')
+    #     print([gtw.vname[v] for v in graph.vertices()])
+    #     print('---- edge ----')
+    #     print([gtw.eweight[e] for e in graph.edges()])
