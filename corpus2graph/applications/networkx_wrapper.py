3848aa19c933595a93d39cec6c2a1c9061bb8bc0
Zheng ZHANG <zzcoolj@Zhengs-MacBook-Pro.local>
2019-02-28 10:15:12 +0100

    format

--- corpus2graph/applications/networkx_wrapper.py
+++ corpus2graph/applications/networkx_wrapper.py
@@ -27,15 +27,15 @@ class NetworkXWrapper(object):
         else:
             self.graph.add_edge(w1, w2, weight=1)
 
-    def add_edges_from_file(self, path):
-        # used by our method (corpus2graph)
-        self.graph = nx.read_weighted_edgelist(path, create_using=nx.DiGraph(), nodetype=int)
-
     def add_edges_from_list(self, edges):
         # used by graph_generator.py
         # A little bit better naive way of generating word co-occurrence network, DO NOT USE IT!
         # edges: [(k.split()[0], k.split()[1], d[k]) for k in d]
         self.graph.add_weighted_edges_from(edges)
+
+    def add_edges_from_file(self, path):
+        # used by our method (corpus2graph)
+        self.graph = nx.read_weighted_edgelist(path, create_using=nx.DiGraph(), nodetype=int)
 
     def getGraph(self):
         return self.graph
